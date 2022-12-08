f29bc6a8de3aeb42954dee401b6a91d908146f2a
yuk <2498040006@qq.com>
2022-12-08 17:14:35 +0800

    vue2

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    {{str}}
+    <button @click="btn">点击</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return{
+      name:'jeck'
+    }
+  },
+  props: {
+    msg: String,
+    // str:'test'
+  },
+  methods:{
+    btn(){
+      this.$emit('change',this.name)
+    }
+  },
+  inject:['str']
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App" @change="btn1">
+      插槽传递内容
+    </HelloWorld>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  data(){
+    return{
+      str:"abc"
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  methods:{
+    btn1(val){
+      console.log(val);
+    }
+  },
+  provide(){
+    return{
+      str:this.str
+    }
+  }
+}
+</script>
